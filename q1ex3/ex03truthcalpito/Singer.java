15c5d1524a2c9aa70df49d24288325d189a3d4b0
wrynahcalpito <96109858+wrynahcalpito@users.noreply.github.com>
2022-09-20 15:01:40 +0800

    Update Singer.java

--- q1ex3/ex03truthcalpito/Singer.java
+++ q1ex3/ex03truthcalpito/Singer.java
@@ -14,16 +14,16 @@ public class Singer {
     double earnings;
     Song favoriteSong;
     
-    public Singer (String name, int noOfPerformances, double earnings, Song favoriteSong) {
-        this.name = name;
-        this.noOfPerformances = noOfPerformances;
-        this.earnings = earnings;
+    public Singer (String name, Song favoriteSong) {
+        this.name = name; 
         this.favoriteSong = favoriteSong;
+        this.noOfPerformances = 0;
+        this.earnings = 0;
     }
     
     //get information
     public void getInfo() {
-        System.out.printf("\n%s has performed %d time(s) and has %.2f total earning(s). His favorite song is %s in the key of %c and lasting for %.2f minutes. It is from the album %s which has %d songs and lasts for %.2f minutes.\n", name, noOfPerformances, earnings, favoriteSong.name, favoriteSong.key, favoriteSong.duration, favoriteSong.fromAlbum.name, favoriteSong.fromAlbum.albumLength, favoriteSong.fromAlbum.duration);
+        System.out.printf("\n%s has performed %d time(s) and has %.2f total earning(s). His favorite song is %s in the key of %c and lasting for %.2f minutes. It is from the album %s.\n", name, noOfPerformances, earnings, favoriteSong.name, favoriteSong.key, favoriteSong.duration, favoriteSong.fromAlbum);
     } 
     
     //increases noOfPerformances by 1 and earnings by 100 for each person (e.g. 5 people results in 500 increase)
@@ -34,7 +34,7 @@ public class Singer {
         System.out.printf("\n%s performed %s for %d person(s). After the performance, they now have %.2f total earning(s) and %d performance(s) in their career.\n", name, favoriteSong.name, noOfPeople, earnings, noOfPerformances);
     } 
     
-    public void changeFavSong(String name, double duration, char key, Album fromAlbum) {
+    public void changeFavSong(String name, double duration, char key, String fromAlbum) {
         favoriteSong = new Song(name, duration, key, fromAlbum);
     } 
 }
