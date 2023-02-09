9e0531d969c69a6c2e36b32c340854993488d7fc
wrynahcalpito <96109858+wrynahcalpito@users.noreply.github.com>
2023-02-09 22:16:26 +0800

    Update Subject.java

--- q3ex1/Subject.java
+++ q3ex1/Subject.java
@@ -1,13 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package exercise11truthcalpitowrynahdale;
 
-/**
- *
- * @author TRUTH
- */
 public class Subject {
     private String name, imgFileName;
     private double units, grade;
